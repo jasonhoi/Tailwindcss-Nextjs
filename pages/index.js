38ddf93e361d90aa6436f659a9fcb63ea183fa24
Jason Hoi <flashjx1985@gmail.com>
2022-01-11 10:38:32 +0800

    tailwind container

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,11 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <h1 className='text-3xl font-bold underline font-mono'>Hello World</h1>
+    <div className="container bg-indigo-500 mx-auto">
+      <p className='font-mono text-3xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Aperiam dolor magnam eveniet doloremque omnis rerum, culpa fuga velit. 
+        Amet ipsa accusantium atque quia aspernatur dolorem doloremque tempore 
+        dolor, doloribus magni.</p>
+    </div>
   )
 }
