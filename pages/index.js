c44cc3657b9cf56b13a39f184c271059b59d703b
Jason Hoi <flashjx1985@gmail.com>
2022-01-12 16:39:24 +0800

    test tailwindcss @apply

--- pages/index.js
+++ pages/index.js
@@ -9,12 +9,7 @@ export default function Home() {
             <label className="block">
                 <span className="block text-sm font-medium text-gray-700">Username</span>
             </label>
-            <input type="text" value="tbone" disabled class="mt-1 block px-3 py-2 bg-white border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400
-              focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-              disabled:bg-gray-50 disabled:text-gray-500 disabled:border-gray-200 disabled:shadow-none
-              invalid:border-pink-500 invalid:text-pink-600
-              focus:invalid:border-pink-500 focus:invalid:ring-pink-500
-            "/>
+            <input type="text" value="input value" class="btn"/>
         </form>
         
         <div className="flex flex-row space-x-4">
