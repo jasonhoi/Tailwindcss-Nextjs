5ca1f806d5aa970b3a6f37dd00174a0407e37ac3
Jason Hoi <jason@de3interactive.com>
2022-02-17 10:04:37 +0800

    merge

--- pages/index.js
+++ pages/index.js
@@ -24,26 +24,11 @@ export default function Home() {
               </div>
             </div>
 
-            <div className="space-x-4">
+            <div className="flex gap-4">
               <button type="button" className="btn-purple">Save changes</button>
               <button className="btn-gray">Cancel</button>
             </div>
         </form>
-<<<<<<< HEAD
-        
-        <div className="flex flex-row gap-2">
-          <button className="leading-5 font-semibold active:bg-violet-800 
-          focus:ring-violet-300 focus:ring focus:outline-none text-white text-sm 
-          bg-violet-500 hover:bg-violet-600 text-center rounded-full px-5 py-2">Button</button>
-          <button type="button" className="bg-violet-500 hover:bg-violet-600 focus:outline-none 
-          focus:ring focus:ring-violet-300 active:bg-violet-700 px-5 py-2 text-sm leading-5 
-          rounded-full font-semibold text-white">
-              Save changes
-          </button>
-        </div>
-        
-=======
->>>>>>> e12d8335ba9815d7632ebb0213b464427d1711d8
     </div>
   )
 }
